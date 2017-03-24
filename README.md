db78efc34b44034f5842b4cba846b976672bf9dd
mattjegan <matthewj.egan@hotmail.com>
2017-03-24 22:27:30 +1100

    add setup, authors, license etc

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # wtfuzz - What The Fuzz
 Wtfuzz is a cli tool used for checking the existance of different types of web resources including webpages, files, api endpoints and more.
 
+# Installation
+Requires Python 3.5+
+```
+pip install wtfuzz
+```
+
 # Usage
 Try find resources from some url
 ```
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(author='Matthew Egan',
+      author_email='matthewj.egan@hotmai.com',
+      description='A cli tool used for checking the existance of different types of web resources',
+      name='wtfuzz',
+      py_modules=[
+          'wtfuzz.wtfuzz',
+      ],
+      entry_points={
+            'console_scripts': [
+                  'wtfuzz = wtfuzz.wtfuzz:main'
+            ]
+      },
+      install_requires=[
+            'crayons==0.1.2',
+            'requests==2.13.0',
+      ],
+      url='https://github.com/mattjegan/wtfuzz',
+      version='0.0.1'
+)
